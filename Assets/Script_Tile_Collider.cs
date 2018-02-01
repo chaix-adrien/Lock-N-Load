983981e86c75702365e4f1d336c2087f30d0a48f
chaix-adrien <adrien.chaix@epitech.eu>
2018-02-01 18:30:47 +0100

    Scripted Tile in Editor

--- Assets/Script_Tile_Collider.cs
+++ Assets/Script_Tile_Collider.cs
@@ -1,35 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-	using UnityEditor;
-#endif
+
 public class Script_Tile_Collider : MonoBehaviour {
 
 	// Use this for initialization
-	Collider2D col;
 	void Start () {
-		col = GetComponent<BoxCollider2D>();
-		col.offset = new Vector2(0.5f, 0.5f);
+		Vector3 pos = transform.localPosition;
+		transform.localPosition = new Vector3(pos.x + 0.5f, pos.y + 0.5f, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
-	#if UNITY_EDITOR
-// The following is a helper that adds a menu item to create a RoadTile Asset
-    [MenuItem( "Assets/Create/Tile_Collider" ) ]
-    public static void CreateTile_Colider()
-    {
-        string path = EditorUtility.SaveFilePanelInProject("Save Tile Collider", "Collider_", "prefab", "Save Tile Collider", "Prefab");
-		if (path != "") {
-			GameObject corePrefab = Resources.Load("Collider") as GameObject;
-			Debug.Log(GameObject.FindGameObjectWithTag("TileCollider"));
-			corePrefab.hideFlags = HideFlags.None;
-			PrefabUtility.CreatePrefab(path, corePrefab);
-		}    
-    }
-#endif
 }
--- Assets/Tiles/Core/ScriptedTile.cs
+++ Assets/Tiles/Core/ScriptedTile.cs
@@ -1,23 +1,53 @@
-﻿using UnityEditor;
-using UnityEngine.Tilemaps;
+﻿using UnityEngine.Tilemaps;
 using UnityEngine;
-using System.Collections;
+#if UNITY_EDITOR
+    using UnityEditor;
+    using System.Collections;
+    using System.IO;
+#endif
 
 public class ScriptedTile : Tile 
 {
-    public Sprite[] m_Sprites;
-    public Sprite m_Preview;
-    // This refreshes itself and other RoadTiles that are orthogonally and diagonally adjacent
- 
+    private bool init = false;
+    
+    //Parametre de base de la tile
+    void OnEnable() {
+        if (!init) {
+            flags = TileFlags.InstantiateGameObjectRuntimeOnly;
+            colliderType = ColliderType.None;
+            init = true;
+        }
+    }
 #if UNITY_EDITOR
-// The following is a helper that adds a menu item to create a RoadTile Asset
-    [MenuItem("Assets/Create/ScriptedTile")]
-    public static void CreateRoadTile()
+    [MenuItem("Assets/Create/Scripted Tile")]
+    public static void CreateScriptedTile()
     {
-        string path = EditorUtility.SaveFilePanelInProject("Save Tile", "New Tile", "Asset", "Save Tile", "Tiles");
-        if (path == "")
-            return;
-    AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<ScriptedTile>(), path);
+        var path = "";
+        var obj = Selection.activeObject;
+        if (obj == null) path = "Assets";
+        else path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
+        if (!Directory.Exists(path)) {
+            string[] tab = path.Split('/');
+            tab[tab.Length - 1] = "";
+            path = string.Join("/", tab);
+        } else
+            path += "/";
+        string[] filter = {"", ""};
+        filter[0] = "Image files";
+        filter[1] = "png,jpg,jpeg";
+        string texture = EditorUtility.OpenFilePanelWithFilters("Select Sprite", "Sprite", filter);
+        ScriptedTile tile = ScriptableObject.CreateInstance<ScriptedTile>();
+        ProjectWindowUtil.CreateAsset(tile, path + "NewTile.Asset");
+        GameObject corePrefab = Resources.Load("Collider") as GameObject;
+		corePrefab.hideFlags = HideFlags.None;
+	    GameObject prefab = PrefabUtility.CreatePrefab(path + "NewTile.prefab", corePrefab);
+        tile.gameObject = prefab;
+        if (texture != "") {
+            texture = "Assets" + texture.Replace(Application.dataPath, "");
+            Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(texture);
+            prefab.GetComponent<SpriteRenderer>().sprite = sprite;
+            tile.sprite = sprite;
+        }
     }
 #endif
 }
