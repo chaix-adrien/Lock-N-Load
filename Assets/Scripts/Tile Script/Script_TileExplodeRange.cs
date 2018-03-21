e579271a9d0a2cefccc9e2776dfe6b5bac1c02f3
chaix-adrien <adrien.chaix@epitech.eu>
2018-03-21 19:24:07 +0100

    explosive base

--- Assets/Scripts/Tile Script/Script_TileExplodeRange.cs
+++ Assets/Scripts/Tile Script/Script_TileExplodeRange.cs
@@ -3,69 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Script_TileExplodeRange : Script_TileHandler {
-	public int range = 1;
-	bool m_Started;
-	public float chance = 0.5f;
-	public Tile toPutOnExplode = null;
-	public Tile toPutElse = null;
-
-	public bool onlyOnFloor = false;
-	public int damageOnExplode = 40;
-	public Color onHitColor = Color.white;
-
-	public bool onWalk = false;
-	public bool onShoot = true;
-	private Tilemap tilemap;
-	private Vector3Int pos;	
-	protected override void Start() {
-		base.Start();
-		Vector2Int posInt = GetComponent<Script_Tile_Collider>().pos;
-		pos = new Vector3Int(posInt.x, posInt.y, 0);
-	}
-
-	public override void getShot(GameObject player, string from, string fromDetails) {
-		if (onShoot)
-			explode();
-	}
-
-	protected override void walkedOnEnter(Collider2D entity) {
-		if (onWalk)
-			Invoke("explode", 0.01f);
-	}
-
-	private void explode() {
-		tilemap = GameObject.FindGameObjectWithTag("Map").GetComponent<Tilemap>();
-		Vector3Int check = Vector3Int.zero;
-		Collider2D[] hitColliders = new Collider2D[10];
-		ContactFilter2D contactFilter = new ContactFilter2D();
-		var ret = Physics2D.OverlapBox(transform.position, new Vector2(1f + range, 1f + range), 0f, contactFilter, hitColliders);
-		for (int i = 0; i < ret; i++) {
-			Collider2D inArea = hitColliders[i];
-			Script_Entity entity = inArea.gameObject.GetComponent<Script_Entity>();
-			if (entity)
-				entity.hit(damageOnExplode, onHitColor, "environement", "explosion");
-		}
-
+public class Script_TileExplodeRange : Script_TileExplosionBase {
+	protected override void launchExplosion() {
+		Vector2Int check = Vector2Int.zero;
 		for (check.x = pos.x - range; check.x <= pos.x + range; check.x++) {
 			for (check.y = pos.y - range; check.y <= pos.y + range; check.y++) {
-				ScriptedTile tile = tilemap.GetTile(new Vector3Int(check.x, check.y, 0)) as ScriptedTile;
-				if (!(check.x == pos.x && check.y == pos.y)) {
-					if (!onlyOnFloor || tile.floor) {
-						if (!tile || tile.canBeExplosed) {
-							if (Random.value <= chance)
-								tilemap.SetTile(new Vector3Int(check.x, check.y, 0), toPutOnExplode);
-							else if (toPutElse)
-								tilemap.SetTile(new Vector3Int(check.x, check.y, 0), toPutElse);
-						}
-					}
-				}
+				explode(check);
 			}	
 		}
-		Invoke("destroySelf", 0.01f);
-	}
-
-	private void destroySelf() {
-		tilemap.SetTile(new Vector3Int(pos.x, pos.y, 0), toPutOnExplode);
 	}
 }
---
+++ Assets/Scripts/Tile Script/Script_TileExplosionBase.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Script_TileExplosionBase : Script_TileHandler {
+
+	public int range = 1;
+	public float chance = 0.5f;
+	public Tile toPutOnExplode = null;
+	public Tile toPutElse = null;
+	public bool onlyOnFloor = false;
+	public int damageOnExplode = 40;
+	public Color onHitColor = Color.white;
+	public bool onWalk = false;
+	public bool onShoot = true;
+	private Tilemap tilemap;
+	protected Vector3Int pos;	
+	protected override void Start() {
+		base.Start();
+		Vector2Int posInt = GetComponent<Script_Tile_Collider>().pos;
+		pos = new Vector3Int(posInt.x, posInt.y, 0);
+		tilemap = GameObject.FindGameObjectWithTag("Map").GetComponent<Tilemap>();
+	}
+
+	public override void getShot(GameObject player, string from, string fromDetails) {
+		if (onShoot)
+			launchExplosion();
+	}
+
+	protected override void walkedOnEnter(Collider2D entity) {
+		if (onWalk)
+			Invoke("launchExplosion", 0.01f);
+	}
+
+	protected virtual void launchExplosion() {
+		explode(new Vector2Int(pos.x, pos.y));
+	}
+
+	protected void explode(Vector2Int check) {
+		if (damageOnExplode > 0) {
+			Collider2D[] hitColliders = new Collider2D[50];
+			ContactFilter2D contactFilter = new ContactFilter2D();
+			var ret = Physics2D.OverlapBox(new Vector3(check.x + 0.5f, check.y + 0.5f, 0), new Vector2(1f, 1f), 0f, contactFilter, hitColliders);
+			for (int i = 0; i < ret; i++) {
+				Collider2D inArea = hitColliders[i];
+				Script_Entity entity = inArea.gameObject.GetComponent<Script_Entity>();
+				if (entity)
+					entity.hit(damageOnExplode, onHitColor, "environement", "explosion");
+			}
+		}
+		ScriptedTile tile = tilemap.GetTile(new Vector3Int(check.x, check.y, 0)) as ScriptedTile;
+		if (!(check.x == pos.x && check.y == pos.y)) {
+			if (!onlyOnFloor || tile.floor) {
+				if (!tile || tile.canBeExplosed) {
+					if (Random.Range(0, 1.0f) <= chance)
+						tilemap.SetTile(new Vector3Int(check.x, check.y, 0), toPutOnExplode);
+					else if (toPutElse)
+						tilemap.SetTile(new Vector3Int(check.x, check.y, 0), toPutElse);
+				}
+			}
+		} else {
+				Invoke("destroySelf", 0.01f);
+		}
+	}
+	
+	private void destroySelf() {
+		if (Random.Range(0, 1.0f) <= chance)
+			tilemap.SetTile(new Vector3Int(pos.x, pos.y, 0), toPutOnExplode);
+		else if (toPutElse)
+			tilemap.SetTile(new Vector3Int(pos.x, pos.y, 0), toPutElse);
+	}
+}
