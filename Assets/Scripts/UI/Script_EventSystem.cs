69acf2d3e68564eab9e974044013884e963e2474
chaix-adrien <adrien.chaix@epitech.eu>
2018-05-10 12:15:49 +0200

    Press A to navigate in menu

--- Assets/Scripts/UI/Script_EventSystem.cs
+++ Assets/Scripts/UI/Script_EventSystem.cs
@@ -11,9 +11,25 @@ public class Script_EventSystem : MonoBehaviour {
 	void Update () {
 		if (EventSystem.current.currentSelectedGameObject == null)
 			EventSystem.current.SetSelectedGameObject(lastSelec);
+		if (XCI.GetButtonDown(XboxButton.A)) {
+			if (EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>().GetType() == typeof(Slider)) {
+				Selectable current = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+				EventSystem.current.sendNavigationEvents = false;
+				if (current.FindSelectableOnRight())
+					EventSystem.current.SetSelectedGameObject(current.FindSelectableOnRight().gameObject);
+				else if (current.FindSelectableOnDown())
+					EventSystem.current.SetSelectedGameObject(current.FindSelectableOnDown().gameObject);
+				Invoke("resetEvent", 0.1f);
+			}
+				
+		}
 		if (EventSystem.current.currentSelectedGameObject != lastSelec && lastSelec != null) {
 			XCIextention.SetVibration((XboxController)1, 0.1f, 0.1f);
 		}
 		lastSelec = EventSystem.current.currentSelectedGameObject;
+	}
+
+	private void resetEvent() {
+		EventSystem.current.sendNavigationEvents = true;
 	}
 }
