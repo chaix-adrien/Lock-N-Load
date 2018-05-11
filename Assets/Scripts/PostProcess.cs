4a6e2e82c0f33bb2ae88277d6ca4088027ceb3c5
chaix-adrien <adrien.chaix@epitech.eu>
2018-05-11 18:02:45 +0200

    PostProcess copy persistent save

---
+++ Assets/Scripts/PostProcess.cs
@@ -0,0 +1,23 @@
+﻿// C# example:
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class MyBuildPostprocessor {
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+       var path = "";
+	   if (EditorUserBuildSettings.activeBuildTarget.ToString() == "StandaloneOSX") {
+			path = pathToBuiltProject;
+			path += "/Contents/";
+		} else {
+			var splited = pathToBuiltProject.Split('/');
+			ArrayUtility.RemoveAt(ref splited, splited.Length - 1);
+			path = string.Join("/", splited);
+			path += "/LockNLoad_Data/";
+		}
+		FileUtil.CopyFileOrDirectory(Application.dataPath + "/PersistentSave/", path + "PersistentSave/");
+    }
+}
+#endif
